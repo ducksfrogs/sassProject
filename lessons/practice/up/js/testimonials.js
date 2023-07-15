d644fc1c87e80be9455d68e027330645bc87c5f2
ducksfrogs <ma_yamaki@yahoo.com>
2023-07-15 09:12:35 +0900

    testamonials fin

--- lessons/practice/up/js/testimonials.js
+++ lessons/practice/up/js/testimonials.js
@@ -2,6 +2,22 @@ const slides = document.querySelector('.testimonials__content--slider').children
 
 const indicatorImgs = document.querySelector(".testimonials__content--indicator").children;
 
-const length = indicatorImgs.length;
-console.log(length);
 console.log(indicatorImgs[0]);
+
+for (let i = 0; i < indicatorImgs.length; i++) { 
+    indicatorImgs[i].addEventListener('click', function() {
+        // console.log(this.getAttribute('data-id'));
+        for (let j = 0; j < indicatorImgs.length; j++) {
+            indicatorImgs[j].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        const id = this.getAttribute('data-id');
+        for (let k = 0; k < slides.length; k++) {
+            slides[k].classList.remove('active');
+        }
+
+        slides[id].classList.add('active');
+    }   );
+    
+}
